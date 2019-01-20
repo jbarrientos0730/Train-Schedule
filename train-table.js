9fbef6679d09aecb01b943926faec913431a5534
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2019-01-19 23:25:53 -0600

    Html finished

---
+++ train-table.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC0SBTHqCXRTU70HHH3ny_70y3YjqeyzGs",
+    authDomain: "mytraintimetable.firebaseapp.com",
+    databaseURL: "https://mytraintimetable.firebaseio.com",
+    projectId: "mytraintimetable",
+    storageBucket: "",
+    messagingSenderId: "589133943418"
+  };
+  firebase.initializeApp(config);
