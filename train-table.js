0eba3636d67040fa2ae6b61dc9de3f447c69bc37
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2019-01-19 23:32:59 -0600

    firebase linked

--- train-table.js
+++ train-table.js
@@ -1,10 +1,29 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC0SBTHqCXRTU70HHH3ny_70y3YjqeyzGs",
-    authDomain: "mytraintimetable.firebaseapp.com",
-    databaseURL: "https://mytraintimetable.firebaseio.com",
-    projectId: "mytraintimetable",
-    storageBucket: "",
-    messagingSenderId: "589133943418"
-  };
-  firebase.initializeApp(config);
+var config = {
+apiKey: "AIzaSyC0SBTHqCXRTU70HHH3ny_70y3YjqeyzGs",
+authDomain: "mytraintimetable.firebaseapp.com",
+databaseURL: "https://mytraintimetable.firebaseio.com",
+projectId: "mytraintimetable",
+storageBucket: "",
+messagingSenderId: "589133943418"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var frequency = 0;
+
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+
+    database.ref()push({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency
+    });
+});
